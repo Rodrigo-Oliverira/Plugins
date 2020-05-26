1f2d14efd9f3d1ec1732fdfe118987bc51e55205
Rodrigo-Oliverira <oliver13.silva@gmail.com>
2020-05-26 11:57:30 +0900

    Botão de confirma e ação de decisão

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,30 @@
 // This is a JavaScript file
 
+$(document).on("click","#alerta",function() {
+
+function retorno() {
+    // do something
+}
+
+navigator.notification.alert(
+    'Este é um teste de alerta!',  // message
+    retorno ,       // callback
+    "Atenção:",
+    "OK" 
+
+);
+});
+
+$(document) .on("click", "#confirma",function() {
+
+  function confirmando(buttonIndex) {
+    alert('Você selecionou ' + buttonIndex);
+}
+
+navigator.notification.confirm(
+  "O Palmeiras tem Mundial?",
+  confirmando, 
+  "Pergunta:",
+  ["Sim","Não","Talvez"]
+);
+});
